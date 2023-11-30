68cdeecba11329ff2cf7fa2c6a8639287b5221c4
Goktug Erol <ge_dev@yahoo.com>
2023-11-30 02:09:14 +0000

    n dimensional arrays, first commit

--- n_dimensional_arrays/n_dim_one.py
+++ n_dimensional_arrays/n_dim_one.py
@@ -1,1 +1,26 @@
+import numpy as np
 
+my_array = np.arange(8)     # 8 is stop value
+
+print(my_array)
+print(type(my_array))
+
+# What if we wanna start from certain number / 1 instead of 0?
+# Then we need to assign 2 parameters.
+
+my_new_array = np.arange(1, 8)
+
+print(my_new_array)
+
+# Lets use only odd numbers, specify 3 arguments
+# last argument is step.
+my_odd_array = np.arange(1, 8, 2)
+
+
+my_even_array = np.arange(0, 8, 2)
+my_float_array = np.arange(0, 8, 0.5)
+print(my_odd_array)
+print("Floating point", my_float_array)
+print("Floating", type(my_float_array))
+print(type(my_odd_array))
+print(type(my_even_array))
